230512777d46e18e272a96b7177aebca6fca9e01
Sanjay Singh <sanjaysingh0615@gmail.com>
2025-08-31 23:46:42 +0530

    Fix ALLOWED_HOSTS

--- backend/settings.py
+++ backend/settings.py
@@ -27,8 +27,10 @@ SECRET_KEY = 'django-insecure-fm)3f96&-=$2ue$h(pd1g-z+23!xa11i#*+#1l^6=1nz$bzs_2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    'transportbrokerapp-backend.onrender.com',  # Add your Render URL
+    'localhost',  # optional for local testing
+]
 
 # Application definition
 
